b0bee28af310f11415d8b9042325137ae1de6579
sulenchy <sulaimanabiodun172@gmail.com>
2020-05-17 22:14:59 +0100

    add a todo

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom'
 
 import App from './components/App';
 
+// TODO: create another branch and convert the language to ts(typescript)
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
